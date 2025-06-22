ecd8ea4658fc50faf49fbd0a21de5fe830bb1778
Sean Fontaine <sfontaine20@ucla.edu>
2025-06-22 16:40:14 +0800

    Testing port

--- main.py
+++ main.py
@@ -157,5 +157,6 @@ async def predict_teammates_endpoint(req: TeammateRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
+    print("HELLO")
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
